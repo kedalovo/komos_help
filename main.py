1ca35cf4e46b18736f572a9b5e1ad7af51e06cbb
kedalovo <kedalovo@gmail.com>
2024-08-15 15:26:11 +0400

    fdsfdsf

--- main.py
+++ main.py
@@ -9,6 +9,9 @@ bot = TeleBot("7518293362:AAG-3fDbEwfo65fJj3zuBeV1SAPh4idqlLw")
 
 @bot.message_handler(commands=["start"])
 def start(message: types.Message):
+    # result = requests.get("http://tst-izm-1c001.komos-group.ru/1cv8_ki_for_hakaton_ZykovSL_do18082024/hs/Return/", params=["7288248016"])
+    # print(result.status_code)
+    # print(result.text)
     keys: types.InlineKeyboardMarkup = types.InlineKeyboardMarkup()
     keys.add(types.InlineKeyboardButton(EMOJI_SOS + " Помогите мне! " + EMOJI_SOS, "t.me/komos_help_bot/helpapp"))
     bot.send_message(message.chat.id, EMOJI_DOWN + " Я могу Вам помочь, осталось только нажать на кнопку! " + EMOJI_DOWN, reply_markup=keys)
--- script.js
+++ script.js
@@ -7,11 +7,11 @@ user = app.initDataUnsafe.user
 // ID - user.id, имя - user.first_name, фамилия - user.last_name
 // e-mail - input_email.value
 
-check_user(user.id)
+check_user()
 
 function check_user()
 {
-    let user_id = 7288248016 
+    let user_id = 7288248016
     const xhr = new XMLHttpRequest();
     xhr.open("GET", "http://tst-izm-1c001.komos-group.ru/1cv8_ki_for_hakaton_ZykovSL_do18082024/hs/Return/" + user_id);
     xhr.send();
