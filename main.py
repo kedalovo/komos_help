ba7deb889987fbdfc43e77d9468db3d7b0ec3120
kedalovo <kedalovo@gmail.com>
2024-08-15 14:35:17 +0400

    Убрал лишний код

--- main.py
+++ main.py
@@ -1,13 +1,6 @@
 from telebot import types
 from telebot import TeleBot
-from flask import Flask, request
 import requests
-
-app = Flask(__name__)
-api_url = f"https://api.telegram.org/bot<7518293362:AAG-3fDbEwfo65fJj3zuBeV1SAPh4idqlLw>/setWebhook"
-webhook_url = "https://kedalovo.github.io/komos_help/test"
-response = requests.post(api_url, json={"url": webhook_url})
-print(response.json)
 
 EMOJI_SOS = "🆘"
 EMOJI_DOWN = "🔽"
@@ -20,17 +13,4 @@ def start(message: types.Message):
     keys.add(types.InlineKeyboardButton(EMOJI_SOS + " Помогите мне! " + EMOJI_SOS, "t.me/komos_help_bot/helpapp"))
     bot.send_message(message.chat.id, EMOJI_DOWN + " Я могу Вам помочь, осталось только нажать на кнопку! " + EMOJI_DOWN, reply_markup=keys)
 
-@bot.message_handler(commands=["test"])
-def help(message: types.Message):
-    new_json = {"test_message": "Hello, World!"}
-    url = "https://api.telegram.org/bot7518293362:AAG-3fDbEwfo65fJj3zuBeV1SAPh4idqlLw/test1"
-    x = requests.post(url, json=new_json)
-    print(x.status_code)
-
-@app.route("/test", methods=["POST"])
-def handle_post():
-    data = request.json
-    print(data)
-
-app.run(debug=True)
 bot.infinity_polling()
--- script.js
+++ script.js
@@ -11,35 +11,3 @@ function check_user(id, data)
 {
     // Код, который делает что-то с принятыми данными
 }
-
-function test1(new_json)
-{
-    console.log(new_json["test_message"])
-}
-
-async function sendData(data) {
-    const response = await fetch("https://kedalovo.github.io/komos_help/test", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify(data)
-    });
-    const result = await response.text()
-    console.log(result)
-}
-
-async function handleSubmit() {
-    let data = {
-        user: "someuser",
-        message: "Hello, World!"
-    };
-
-    try {
-        let response = await sendData(data);
-        console.log("Server response: ", response)
-    }
-    catch (error) {
-        console.error("Error: ", error)
-    }
-}
