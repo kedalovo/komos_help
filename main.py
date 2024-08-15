60a009e1992d161adb5c6dcf9ae99b20afcf2f9e
kedalovo <kedalovo@gmail.com>
2024-08-15 14:18:22 +0400

    Тестируем отправку запроса

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 from telebot import types
 from telebot import TeleBot
+from flask import Flask, request
 import requests
+
+app = Flask(__name__)
+api_url = f"https://api.telegram.org/bot<7518293362:AAG-3fDbEwfo65fJj3zuBeV1SAPh4idqlLw>/setWebhook"
+webhook_url = "https://kedalovo.github.io/komos_help/test"
+response = requests.post(api_url, json={"url": webhook_url})
+print(response.json)
 
 EMOJI_SOS = "🆘"
 EMOJI_DOWN = "🔽"
@@ -16,8 +23,14 @@ def start(message: types.Message):
 @bot.message_handler(commands=["test"])
 def help(message: types.Message):
     new_json = {"test_message": "Hello, World!"}
-    url = "https://api.telegram.org/7518293362:AAG-3fDbEwfo65fJj3zuBeV1SAPh4idqlLw/test"
+    url = "https://api.telegram.org/bot7518293362:AAG-3fDbEwfo65fJj3zuBeV1SAPh4idqlLw/test1"
     x = requests.post(url, json=new_json)
     print(x.status_code)
 
+@app.route("/test", methods=["POST"])
+def handle_post():
+    data = request.json
+    print(data)
+
+app.run(debug=True)
 bot.infinity_polling()
