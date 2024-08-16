6f11756cbe2597666f2f070ed3980d450c525153
kedalovo <kedalovo@gmail.com>
2024-08-16 09:13:40 +0400

    Попытка пофиксить кнопку

--- script.js
+++ script.js
@@ -1,19 +1,20 @@
-app = window.Telegram.WebApp
+// app = window.Telegram.WebApp
 
 input_email = document.getElementsByName("e-mail")
 input_code = document.getElementsByName("confirmation-code")
 
-user = app.initDataUnsafe.user
+// user = app.initDataUnsafe.user
 // ID - user.id, имя - user.first_name, фамилия - user.last_name
 // e-mail - input_email.value
 
-check_user()
-
 function check_user()
 {
-    let user_id = 7288248016
+    app = window.Telegram.WebApp
+    user = app.initDataUnsafe.user
+    console.log("TESTING")
     const xhr = new XMLHttpRequest();
-    xhr.open("GET", "http://tst-izm-1c001.komos-group.ru/1cv8_ki_for_hakaton_ZykovSL_do18082024/hs/Return/" + user_id);
+    xhr.open("GET", "http://tst-izm-1c001.komos-group.ru/1cv8_ki_for_hakaton_ZykovSL_do18082024/hs/UserData/Data?UserId=", toString(7288248016));
+    xhr.setRequestHeader("Access-Control-Allow-Origin", "True")
     xhr.send();
     xhr.responseType = "json";
     xhr.onload = () => {
@@ -24,3 +25,5 @@ function check_user()
     }
     };
 }
+
+check_user()
